d1d60e865df6d443ab7cb3ef33ef15ecb5bd3566
ZuraJeladze2 <zurajeladze2@gmail.com>
2024-05-24 02:56:16 +0400

    adding user.service

---
+++ src/app/services/user.service.ts
@@ -0,0 +1,34 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable, inject } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment.development';
+import { User } from '../interfaces/user.interface';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+  apiUrl = environment.apiUrl;
+
+  http = inject(HttpClient);
+
+  getUsers(): Observable<User[]> {
+    return this.http.get<User[]>(`${this.apiUrl}/users`);
+  }
+
+  getUser(id: string): Observable<User> {
+    return this.http.get<User>(`${this.apiUrl}/users/${id}`);
+  }
+
+  createUser(user: Partial<User>): Observable<User> {
+    return this.http.post<User>(`${this.apiUrl}/users`, user);
+  }
+
+  updateUser(user: User): Observable<User> {
+    return this.http.put<User>(`${this.apiUrl}/users/${user.id}`, user);
+  }
+
+  deleteUser(id: string): Observable<User> {
+    return this.http.delete<User>(`${this.apiUrl}/users/${id}`);
+  }
+}
