38c6584317aaf82b23198e6f80400b52b399b729
ZuraJeladze2 <zurajeladze2@gmail.com>
2024-05-24 02:48:52 +0400

    compontents -> pages
    renaming components to pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,7 @@
 import { Routes } from '@angular/router';
-import {UserCreateComponent} from './components/user-create/user-create.component'
-import { UsersComponent } from './components/users/users.component';
-import { UserDetailedComponent } from './components/user-detailed/user-detailed.component';
+import {UserCreateComponent} from './pages/user-create/user-create.component'
+import { UsersComponent } from './pages/users/users.component';
+import { UserDetailedComponent } from './pages/user-detailed/user-detailed.component';
 
 export const routes: Routes = [
     {
