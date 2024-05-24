ff06dfce5aa5925f63c6c71ad92c41ceaadfe6a1
ZuraJeladze2 <Zurajeladze2@gmail.com>
2024-05-24 20:31:46 +0400

    adding user edit
    user edit works. also incorrect paths redirect to main page.
    
    And a lot of bugs are fixed.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import {UserCreateComponent} from './pages/user-create/user-create.component'
 import { UsersComponent } from './pages/users/users.component';
 import { UserDetailedComponent } from './pages/user-detailed/user-detailed.component';
+import { Page404Component } from './pages/page404/page404.component';
 
 export const routes: Routes = [
     {
@@ -19,5 +20,9 @@ export const routes: Routes = [
     {
       path: 'view/:id',
       component: UserDetailedComponent
+    },
+    {
+      path: '**',
+      redirectTo: ''
     }
   ];
