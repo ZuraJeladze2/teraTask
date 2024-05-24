07abbd5a8e76823876b581857c39d18d1c53f2cd
ZuraJeladze2 <Zurajeladze2@gmail.com>
2024-05-24 06:38:39 +0400

    adding register
    register / user create works

--- src/app/pages/user-create/user-create.component.ts
+++ src/app/pages/user-create/user-create.component.ts
@@ -1,12 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { UserService } from '../../services/user.service';
+import { Router } from '@angular/router';
+import { switchMap } from 'rxjs';
 
 @Component({
   selector: 'app-user-create',
   standalone: true,
-  imports: [],
+  imports: [MatFormFieldModule, ReactiveFormsModule, MatIconModule, MatButtonModule, MatInputModule],
   templateUrl: './user-create.component.html',
   styleUrl: './user-create.component.scss'
 })
 export class UserCreateComponent {
+  userForm: FormGroup = new FormGroup({
+    name: new FormControl('', [Validators.required, Validators.minLength(2)]),
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [Validators.required, Validators.minLength(4)])
+  })
+  router = inject(Router)
+  userService = inject(UserService)
 
+  registerUser() {
+    this.userService.createUser(this.userForm.getRawValue()).subscribe(x => {
+      return this.router.navigate(['']);
+    })
+  }
 }
