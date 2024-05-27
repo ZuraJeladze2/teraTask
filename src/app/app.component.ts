09a962365d1863d202c4d317159269ffa720b818
ZuraJeladze2 <Zurajeladze2@gmail.com>
2024-05-28 00:49:41 +0400

    sidebar & navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,34 @@
 import { Component, inject } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatDrawer, MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { RouterOutlet } from '@angular/router';
+import { BtnComponent } from "./components/btn/btn.component";
+import { IconComponent } from "./components/icon/icon.component";
 import { AuthService } from './services/auth.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  template: `<router-outlet/>`,
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss',
+  imports: [
+    RouterOutlet,
+    MatSidenavModule, MatInputModule, MatIconModule, MatButtonModule, MatToolbarModule, MatDrawer,
+    BtnComponent, IconComponent
+  ]
 })
 export class AppComponent {
   title = 'UserManagement';
   authService = inject(AuthService)
   currentUser$ = this.authService.currentUser$;
 
-  ngOnInit(){
+  ngOnInit() {
     this.currentUser$.subscribe(x => {
       console.log('current user is ', x);
-      
+
     })
   }
 }
