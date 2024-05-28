b2615607e62b36f83a942e6fb564fa0478cb6edd
ZuraJeladze2 <Zurajeladze2@gmail.com>
2024-05-28 22:35:32 +0400

    correcting imports in user facade

--- src/app/facades/user.facade.ts
+++ src/app/facades/user.facade.ts
@@ -2,17 +2,17 @@ import { Injectable, inject } from '@angular/core';
 import { BehaviorSubject, Observable } from 'rxjs';
 import { tap } from 'rxjs/operators';
 import { User } from '../interfaces/user.interface';
-import { UserStateService } from '../services/user-state.service';
 import { UserService } from '../services/user.service';
+import { UserStateFacade } from './user-state.facade';
 
 @Injectable({
   providedIn: 'root'
 })
-export class UserFacadeService {
+export class UserFacade {
   private usersSubject = new BehaviorSubject<User[]>([]);
   users$ = this.usersSubject.asObservable();
   private userService = inject(UserService);
-  private userStateService = inject(UserStateService);
+  private userStateFacade = inject(UserStateFacade);
 
   constructor() {
     this.loadUsers();
@@ -43,9 +43,9 @@ export class UserFacadeService {
     return this.userService.updateUser(user).pipe(
       tap((updatedUser) => {
         this.loadUsers();
-        const currentUser = this.userStateService.getCurrentUser();
+        const currentUser = this.userStateFacade.getCurrentUser();
         if (currentUser && currentUser.id === updatedUser.id) {
-          this.userStateService.setCurrentUser(updatedUser);
+          this.userStateFacade.setCurrentUser(updatedUser);
         }
       })
     )
