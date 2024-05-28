aa441c5c4cc823a269545452c49dfdafa108c13e
ZuraJeladze2 <Zurajeladze2@gmail.com>
2024-05-28 12:02:39 +0400

    lazy loading and preloadAll

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,10 +1,10 @@
 import { ApplicationConfig } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { PreloadAllModules, provideRouter, withPreloading } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideHttpClient(), provideAnimationsAsync(), provideAnimationsAsync(), provideAnimationsAsync(), provideAnimationsAsync()]
+  providers: [provideRouter(routes, withPreloading(PreloadAllModules)), provideHttpClient(), provideAnimationsAsync()]
 };
