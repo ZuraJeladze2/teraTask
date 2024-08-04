4de5b1232a80928b9677a8b968cfd0e74873b907
ZuraJeladze2 <Zurajeladze2@gmail.com>
2024-08-04 14:18:18 +0400

    contact page
    contact form for sending messages

--- src/app/pages/about/about.component.ts
+++ src/app/pages/about/about.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import {MatCardModule} from '@angular/material/card';
+import { MatDivider } from '@angular/material/divider';
 
 @Component({
   selector: 'app-about',
   standalone: true,
-  imports: [MatCardModule],
+  imports: [MatCardModule, MatDivider],
   templateUrl: './about.component.html',
   styleUrl: './about.component.scss'
 })
--- src/app/pages/contact/contact.component.ts
+++ src/app/pages/contact/contact.component.ts
@@ -1,9 +1,17 @@
 import { Component } from '@angular/core';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { FormsModule } from '@angular/forms';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDivider } from '@angular/material/divider';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [],
+  imports: [
+    FormsModule, MatFormFieldModule, MatInputModule, MatCardModule, MatButtonModule, MatDivider
+  ],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.scss'
 })
